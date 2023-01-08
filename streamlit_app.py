8104afd8a45021a457003fc150d4b7f600b09d68
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-08 21:11:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 
 streamlit.header('Breakfast Favourites')
@@ -8,3 +9,5 @@ streamlit.text('🌯Hard-Boiled Free-Range Egg')
 streamlit.text('🍪🥑Avacado Toast')
 
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
