c1a564fcad2c2956fd35a703d478e21fe6b1d719
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-08 21:18:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ streamlit.text('🍪🥑Avacado Toast')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avacado','Straweberry'])
+streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberry'])
 streamlit.dataframe(my_fruit_list)
