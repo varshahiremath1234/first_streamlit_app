540079ae384b77fa1fa29835032a04640f2e3c44
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-08 21:05:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
+import pandas
 
-import streamlit
-streamlit.title("Hello StreamLit!")
-streamlit.header("This is new app")
-streamlit.header('Breakfast Menu')
-streamlit.text(🥗'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥘'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🌯'Hard-Boiled Free-Range Egg')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
