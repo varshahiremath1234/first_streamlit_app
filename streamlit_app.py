2ed084aeb45d421e138296a9c087c44739392223
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-08 21:10:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍠Kale, Spinach & Rocket Smoothie')
 streamlit.text('🌯Hard-Boiled Free-Range Egg')
-streamlit.test('🍪🥑Avacado Toast')
+streamlit.text('🍪🥑Avacado Toast')
 
 
