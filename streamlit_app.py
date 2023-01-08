22d78dbc00374424988c560ccd06609ad9ba2428
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-08 21:15:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ streamlit.text('🌯Hard-Boiled Free-Range Egg')
 streamlit.text('🍪🥑Avacado Toast')
 
 
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
