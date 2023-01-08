609bc78a4567bb4bea6171413b75a0fe5533fa6e
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-08 20:56:39 +0530

    Update streamlit_app.py
    Added more headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 streamlit.title("Hello StreamLit!")
 streamlit.header("This is new app")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
