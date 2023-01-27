3b423755b511f947b9e1b5f772c15279e705239e
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-28 00:06:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ streamlit.text('🍠Kale, Spinach & Rocket Smoothie')
 streamlit.text('🌯Hard-Boiled Free-Range Egg')
 streamlit.text('🍪🥑Avacado Toast')
 
+streamlit.header('Fruity Vice Fruit Advice')
+
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -19,7 +21,7 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
