b8182e851b5d73e0a07e621d19dd28440286a255
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-29 22:18:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,5 @@ try:
     back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
-                                     #streamlit.write('The user entered ', fruit_choice)
-
 
 
