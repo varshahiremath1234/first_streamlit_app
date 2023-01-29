8809d7d450a926119447e45450f6697a6f8d8a9b
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-29 22:15:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,13 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
   fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
+
 streamlit.header('Fruity Vice Fruit Advice')
-    
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information.")
-  else:
+else:
     back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
