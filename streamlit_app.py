67752a0c5c7cb10fe8466c23599d638d528bf928
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-29 22:54:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,31 +3,9 @@ import pandas
 import requests
 import snowflake.connector
 
-streamlit.header('Breakfast Favourites')
-streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
-streamlit.text('🍠Kale, Spinach & Rocket Smoothie')
-streamlit.text('🌯Hard-Boiled Free-Range Egg')
-streamlit.text('🍪🥑Avacado Toast')
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-fruits_selected=streamlit.multiselect("Pick some fruits here:",list(my_fruit_list.index),['Pear','Strawberries'])
-fruits_to_show=my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
-
-
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
-  fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
-
-streamlit.header('Fruity Vice Fruit Advice')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function=get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
- 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
