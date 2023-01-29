13485597af7bc6b116469b09ab93cab34e63f39f
varshahiremath1234 <122118322+varshahiremath1234@users.noreply.github.com>
2023-01-29 22:29:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,6 @@ streamlit.text('🍠Kale, Spinach & Rocket Smoothie')
 streamlit.text('🌯Hard-Boiled Free-Range Egg')
 streamlit.text('🍪🥑Avacado Toast')
 
-
-
-
-
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -35,3 +30,4 @@ try:
   else:
     back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
+ 
